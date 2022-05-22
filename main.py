b846df220c9b26c3c581a8e8ec6c9a78cc0d2eb5
FrankSpinachi <76732249+FrankSpinachi@users.noreply.github.com>
2022-05-22 21:14:35 +0200

    v3

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ class Pracownik:
     def wynik(self, imie, wynagrodzenieBrutto):
 
         a = float(wynagrodzenieBrutto)
-        c = round(0.0976*a, 2) + round(0.015*a, 2) + round(0.0245*a, 2)
+        c = round(round(0.0976*a, 2) + round(0.015*a, 2) + round(0.0245*a, 2),2)
         d = a - c
         e = round(d * 0.09, 2)
         f = round(d * 0.0775, 2)
         g = 111.25
         h = round(a - g - c, 2)
         i = round((h*0.18)-46.33,2)
-        j = round(i - f,2) 
+        j = round(i - f) 
         k = round(a-c-e-j,2) # źle się liczy 
         l = round(0.0976*a, 2) + round(a*0.065,2) + round(0.0193*a, 2) + round(0.0245*a, 2) + round(0.001*a, 2)  
         ł = round(a+l,2)
@@ -22,15 +22,15 @@ class Pracownik:
     
     def koszt_pracodawcy_per_pracownik(self,wynagrodzenieBrutto):
         a = float(wynagrodzenieBrutto)
-        c = round(0.0976*a, 2) + round(0.015*a, 2) + round(0.0245*a, 2)
+        c = round(round(0.0976*a, 2) + round(0.015*a, 2) + round(0.0245*a, 2),2)
         d = a - c
         e = round(d * 0.09, 2)
         f = round(d * 0.0775, 2)
         g = 111.25
         h = round(a - g - c, 2)
-        i = round(round(h*0.18)-46.33,2)
-        j = round(i - f,2)
-        k = round(a-c-e-j,2)
+        i = round((h*0.18)-46.33,2)
+        j = round(i - f) 
+        k = round(a-c-e-j,2) # źle się liczy 
         l = round(0.0976*a, 2) + round(a*0.065,2) + round(0.0193*a, 2) + round(0.0245*a, 2) + round(0.001*a, 2)  
         ł = round(a+l,2)
         return ł
@@ -45,15 +45,6 @@ for i in range(liczba_pracowników):
     prac = input().strip()
     prac = prac.split(' ')
     lista_pracowników.append(Pracownik(prac[0],prac[1]))
-
-# for i in range(liczba_pracowników):
-#     prac = input().strip()
-#     prac = prac.split(' ')
-#     print("prac:",prac)
-#     for j in range(len(prac)):
-#         print("prac0",prac[0],"prac1",prac[1])
-#         lista_pracowników.append(Pracownik(prac[0],prac[1]))
-
 
 łączny_koszt_pracodawcy = 0
 
